4471d2d61054d6117a7fcad4bd3cb9b573c7219a
kacper-j-ostrowski <kacper.j.ostrowski@gmail.com>
2021-10-16 14:34:18 +0200

    SDA-HIBERNATE SecondaryTable - odczyt

--- src/main/java/pl/sda/hibernate/Hibernate.java
+++ src/main/java/pl/sda/hibernate/Hibernate.java
@@ -1,8 +1,11 @@
 package pl.sda.hibernate;
 
 import org.hibernate.Session;
+import org.hibernate.query.Query;
 import pl.sda.hibernate.entity.Book;
 import pl.sda.hibernate.utils.HibernateUtil;
+
+import java.util.List;
 
 public class Hibernate {
 
@@ -10,14 +13,14 @@ public class Hibernate {
         Session session = HibernateUtil.getSessionFactory().openSession();
         session.beginTransaction();
 
-        // stworzenie encji, ktora zapiszemy w bazie danych
-        Book book = new Book();
-        book.setId(1);
-        book.setTitle("Harry Potter");
-        book.setType("fantasy");
-
-        // ZAPIS DO BAZY DANYCH
-        session.save(book);
+        // WYKONIANIE ZAPYTANIA. ROWNOZNACZNE Z SELECT * FROM BOOKS
+        Query<Book> result = session.createQuery("from Book WHERE id = 1");
+        // POBRANIE WYNIKU JAKO LISTY
+        List<Book> list = result.list();
+        // ZAKLADAMY, ZE BEDZIE JEDEN WYNIK
+        Book harryPotter = list.get(0);
+        // SPRAWDZAMY TYPE. HIBERNATE POWINIEN ZROBIC JOIN-a TABEL books I book_type
+        System.out.println("Harry Potter type: " + harryPotter.getType());
 
         // POTWIERDZENIE ZAPISU DO BAZY DANYCH - COMMIT
         session.getTransaction().commit();
